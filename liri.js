ee4c218732c7d49e5cac33d943c2ffc63ff12a38
Leon Do <leondo@Leons-MacBook-Pro.local>
2016-12-09 00:40:47 -0500

    got varialbes from keys.js working

--- liri.js
+++ liri.js
@@ -1,1 +1,13 @@
 //write the code you need to grab the data from keys.js. Then store the keys in a variable
+var keys = require('./keys.js');
+
+var twitterConsumerKey = keys.twitterKeys.consumer_key;
+var twitterConsumerSecret = keys.twitterKeys.consumer_secret;
+var twitterAccessTokenKey = keys.twitterKeys.access_token_key;
+var twitterAccessTokenSecret = keys.twitterKeys.access_token_secret;
+
+
+console.log(twitterConsumerKey)
+console.log(twitterConsumerSecret)
+console.log(twitterAccessTokenKey)
+console.log(twitterAccessTokenSecret)
